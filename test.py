ebad9d9079892ff8c8abc47ab47fb1aa1f60d06d
Ketan Aggarwal <ketanaggarwal58@gmail.com>
2023-03-13 22:40:29 +0530

    initial commit

---
+++ test.py
@@ -0,0 +1,11 @@
+# Basic Print In-built Function in Python
+print("Hello World!")
+
+# User Defined - we build this function
+def test(): # Basic Function
+    print("Hello World!")
+    
+# Calling the functions
+test()
+test()
+test()
